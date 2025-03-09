223fec7d31119544b04641476d72f03a44fb4095
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-03-09 08:31:31 +0000

    Starting template no APIs yet

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+
+EXCHANGE_RATE_KEY = ENV.fetch("EXCHANGE_RATE_KEY")
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @currency_list = []
+  @currency_list.push("AED")
+  erb(:main)
+end
+
+get("/:currency_symbol") do
+  @currency_symbol = params.fetch("currency_symbol")
+  @currency_list = []
+  @currency_list.push("MXN")
+
+  erb(:convert_list)
+end
+
+get("/:currency_symbol/:second_currency_symbol") do
+  @currency_symbol = params.fetch("currency_symbol")
+  @second_currency_symbol = params.fetch("second_currency_symbol")
+  @conversion = 23
+  
+  erb(:convert_to)
 end
